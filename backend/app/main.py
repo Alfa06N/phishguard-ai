c0e0fdf247376046934f2c8721dd0c689999bfd7
Alfa06N <nicolasalfag@gmail.com>
2026-05-09 21:33:26 -0400

    add cors specific origins for production and development

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173",
+                   "https://phishguard-ai-kappa.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
